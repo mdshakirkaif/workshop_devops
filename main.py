1f67bd0cb319f2fe06d026e1a9d3a1fa668b2b6f
mdshakirkaif <167667519+mdshakirkaif@users.noreply.github.com>
2025-03-21 12:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ if st.button("Submit"):
 
    st.success("Password is valid")
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+# st.title('Hello World')
+# st.write('This is a simple Streamlit app.')
 
 
-if st.button('Say hello'):
-   st.text('Hello, Streamlit!')
+# if st.button('Say hello'):
+#    st.text('Hello, Streamlit!')
 
 
-name = st.text_input('Please enter your name:')
-if name:
-   st.write(f'Hello, {name}!')
+# name = st.text_input('Please enter your name:')
+# if name:
+#    st.write(f'Hello, {name}!')
 
 
-if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
-   st.write('Thanks for uploading a file!')
+# if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
+#    st.write('Thanks for uploading a file!')
 
 
 
